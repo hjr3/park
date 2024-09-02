f48416f1a57e0aab796e4a951c1d549dc17fb354
Herman J. Radtke III <herman@hermanradtke.com>
2024-09-02 19:59:38 -0400

    quickstart config

--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,19 @@ pub struct Config {
     pub server: Server,
 }
 
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            database: Database {
+                url: "sqlite::memory:".to_string(),
+            },
+            server: Server {
+                addr: "127.0.0.1:0".parse().expect("default address is valid"),
+            },
+        }
+    }
+}
+
 #[derive(Deserialize)]
 pub struct Database {
     pub url: String,
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 use std::sync::Arc;
 
+use clap::{Arg, Command};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper_util::rt::TokioIo;
@@ -8,10 +9,51 @@ use tokio::net::TcpListener;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use park::config::Config;
-use park::config::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let matches = Command::new("park")
+        .about("Generate har files for proxied requests")
+        .arg(
+            Arg::new("url_or_socket")
+                .help("The URL or socket to connect to. Example: http://example.com or 127.0.0.1:8080")
+                .index(1),
+        )
+        .arg(
+            Arg::new("bind")
+                .help("The port or socket to bind to. If IP address is not specified, then 127.0.0.1 is used. Example: 8080 or 127.0.0.1:8080")
+                .index(2)
+        )
+        .arg(
+            Arg::new("config")
+                .short('f')
+                .long("config")
+                .help("Path to the configuration file")
+                .value_name("FILE")
+                .conflicts_with("url_or_socket"),
+        )
+        .get_matches();
+
+    let config: Config = if let Some(url_or_socket) = matches.get_one::<String>("url_or_socket") {
+        let mut config = Config::default();
+        // TODO support full URLs
+        config.server.addr = url_or_socket.parse::<SocketAddr>()?;
+        config
+    } else if let Some(config_file) = matches.get_one::<String>("config") {
+        let content = std::fs::read_to_string(config_file)?;
+        toml::from_str(&content)?
+    } else {
+        eprintln!("You must specify either a domain or a configuration file.");
+        std::process::exit(1);
+    };
+
+    // TODO support full IP addresses
+    let port = if let Some(bind) = matches.get_one::<String>("bind") {
+        bind.parse::<u16>().unwrap_or(0)
+    } else {
+        0
+    };
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -20,14 +62,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let config = Config {
-        database: park::config::Database {
-            url: "sqlite::memory:".to_string(),
-        },
-        server: Server {
-            addr: "127.0.0.1:8080".parse().unwrap(),
-        },
-    };
     let config = Arc::new(config);
     let state = park::AppState {
         db: sqlx::SqlitePool::connect(&config.database.url).await?,
@@ -36,10 +70,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut conn = state.db.acquire().await?;
     sqlx::migrate!().run(&mut conn).await?;
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
 
-    tracing::info!("Listening on {}", addr);
     let listener = TcpListener::bind(addr).await?;
+    tracing::info!("Listening on {}", listener.local_addr()?);
 
     loop {
         let (stream, _) = listener.accept().await?;
